6facce65d89dec2b9740b394c63a134321df6098
siva prakasam <144592981+sivaprakasam-07@users.noreply.github.com>
2023-10-23 15:33:01 +0530

    Update speakGoodBye.js

--- js/speakGoodBye.js
+++ js/speakGoodBye.js
@@ -4,7 +4,7 @@
 	byeSpeaker. speak = function (name) {
 		console.log(speakWord + " " + name);
 	}
-	window.byeSpeaker =  byeSpeaker;
+	window.byeSpeaker =  byeSpeaker; 
 })(window);
 
 // NOTE! The steps in this file are basically identical to the ones you
